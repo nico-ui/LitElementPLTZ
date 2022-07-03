841a870531b12cede1b57eaa6afda60daebf1d84
Nicolai Hernández <cristian.nicolai@outlook.es>
2022-07-03 18:51:00 -0500

    8SLOTTED

---
+++ 8SLOTTED/my-element.js
@@ -0,0 +1,49 @@
+export class MyElement extends HTMLElement {
+
+    constructor() {
+        super();
+        this.attachShadow({mode: "open"});
+    }
+
+    getTemplates() {
+        const template = document.createElement('template');
+        template.innerHTML = `
+            <section>
+                <h2>
+                    <slot name='titulo'></slot>
+                </h2>
+                <div>
+                    <p>
+                        <slot name='parrafo'></slot>
+                    </p>
+                </div>
+            </section>
+            ${this.getStyles()}
+        `;
+        return template;
+    }
+
+    getStyles() {
+        return `
+            <style>
+                ::slotted(span){
+                    font-size: 30px;
+                    color: red;
+                }
+                ::slotted(.text){
+                    color: blue;
+                }
+            </style>
+        `;
+    }
+
+    render() {
+        this.shadowRoot.appendChild(this.getTemplates().content.cloneNode(true));
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+}
+customElements.define('my-element', MyElement);
